4793cd5ed1d5c4d5d7f5e3261d94bb4e3a82284e
Mark <marksreese@gmail.com>
2025-12-08 16:38:08 -0800

    remove files in public/ and fix casing in textnode

--- src/main.py
+++ src/main.py
@@ -5,3 +5,5 @@ print("hello world")
 def main():
     sample_node = TextNode("This is some anchor text", TextType.LINK, "http://boot.dev")
     print(sample_node)
+
+main()
--- src/textnode.py
+++ src/textnode.py
@@ -8,7 +8,7 @@ class TextType(Enum):
     LINK = "link"
     IMAGE = "image"
 
-class textnode():
+class TextNode():
     def __init__(self, text: str, text_type: TextType, url: str = None):
         self.text = text
         self.text_type = text_type
