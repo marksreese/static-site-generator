0ebed9e910c35b1f5b338c8cc5578a665a5c0a96
Mark <marksreese@gmail.com>
2025-12-08 22:04:31 -0800

    create HTMLNode class with finished props_to_html method

---
+++ src/htmlnode.py
@@ -0,0 +1,21 @@
+
+
+class HTMLNode:
+    def __init__(self, tag: str = None, value: str = None, children: list = None, props: dict = None): # type: ignore
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError()
+    
+    def props_to_html(self):
+        if not self.props:
+            return ""
+        
+        html_string = ""
+        for key, value in self.props.items():
+            html_string += f' {key}="{value}"'
+        
+        return html_string
