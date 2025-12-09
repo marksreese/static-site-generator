1d6633ed4f72cc390188073eeec0a63e28ff5316
Mark <marksreese@gmail.com>
2025-12-08 21:55:18 -0800

    write unit tests for textnode.py

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,15 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
+    def test_repr(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        self.assertEqual(repr(node), "TextNode(This is a text node, italic, None)")
+
+    def test_link_node(self):
+        node = TextNode("Click here", TextType.LINK, "http://example.com")
+        self.assertEqual(node.text, "Click here")
+        self.assertEqual(node.text_type.value, "link")
+        self.assertEqual(node.url, "http://example.com")
 
 if __name__ == "__main__":
     unittest.main()
