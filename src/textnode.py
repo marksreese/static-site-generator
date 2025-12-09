4b82f3f87e7c97df8f464ead782521ae88e04462
Mark <marksreese@gmail.com>
2025-12-08 16:27:49 -0800

    write constructor, equality, and print methods for textnode

--- src/textnode.py
+++ src/textnode.py
@@ -1,7 +1,25 @@
 from enum import Enum
 
-class Bender(Enum):
-    AIR_BENDER = "air"
-    WATER_BENDER = "water"
-    EARTH_BENDER = "earth"
-    FIRE_BENDER = "fire"
+class TextType(Enum):
+    PLAIN = "plain"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+class textnode():
+    def __init__(self, text: str, text_type: TextType, url: str = None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, node):
+        return (
+            self.text == node.text and
+            self.text_type == node.text_type and
+            self.url == node.url
+        )
+    
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
