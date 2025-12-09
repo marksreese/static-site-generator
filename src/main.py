7042ac364214d27297d142f2ec4d037fe01f4ab9
Mark <marksreese@gmail.com>
2025-12-08 16:29:54 -0800

    create TextNode with dummy values and print it

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
+from textnode import TextNode, TextType
+
 print("hello world")
+
+def main():
+    sample_node = TextNode("This is some anchor text", TextType.LINK, "http://boot.dev")
+    print(sample_node)
