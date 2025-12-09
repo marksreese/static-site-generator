cc2a810915be8986b3dab956caf8aa90291d8133
Mark <marksreese@gmail.com>
2025-12-08 21:46:19 -0800

    create test script and textnode test files

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
 from textnode import TextNode, TextType
 
-print("hello world")
-
 def main():
     sample_node = TextNode("This is some anchor text", TextType.LINK, "http://boot.dev")
     print(sample_node)
