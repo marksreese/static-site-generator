c441675dd1b7f855627673b6fa3ea1467310cabb
Mark <marksreese@gmail.com>
2026-02-18 14:49:21 -0800

    add block_to_block_type function and corresponding test

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import re
 
 class BlockType(Enum):
     PARAGRAPH = "paragraph"
@@ -18,3 +19,17 @@ def markdown_to_blocks(markdown: str):
         filtered.append(block)
     return filtered
 
+def block_to_block_type(block: str):
+    # check if block starts with 1 to 6 # symbols followed by a space
+    if re.match(r"^#{1,6} ", block):
+        return BlockType.HEADING
+    elif all(line.startswith("- ") for line in block.split("\n")):
+        return BlockType.UNORDERED_LIST
+    elif all(line.startswith(f"{i + 1}. ") for i, line in enumerate(block.split("\n"))):
+        return BlockType.ORDERED_LIST
+    elif all(line.startswith(">") for line in block.split("\n")):
+        return BlockType.QUOTE
+    elif block.startswith("```\n") and block.endswith("```"):
+        return BlockType.CODE
+    else:
+        return BlockType.PARAGRAPH
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,5 +1,5 @@
 import unittest
-from block_markdown import markdown_to_blocks
+from block_markdown import *
 
 class TestMarkdownBlocks(unittest.TestCase):
     def test_extract_blocks_from_markdown(self):
@@ -21,5 +21,13 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+    
+    def test_block_to_block_type(self):
+        self.assertEqual(block_to_block_type("# Heading"), BlockType.HEADING)
+        self.assertEqual(block_to_block_type("1. First item\n2. Second item"), BlockType.ORDERED_LIST)
+        self.assertEqual(block_to_block_type("- First item\n- Second item"), BlockType.UNORDERED_LIST)
+        self.assertEqual(block_to_block_type("> This is a quote"), BlockType.QUOTE)
+        self.assertEqual(block_to_block_type("```\nCode block\n```"), BlockType.CODE)
+        self.assertEqual(block_to_block_type("This is a paragraph."), BlockType.PARAGRAPH)
 
     
