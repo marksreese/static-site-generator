d98dd0105d7e924bdc4e58fc0ceaf66a63a35fea
Mark <marksreese@gmail.com>
2026-02-19 15:56:52 -0800

    write recursive page generation and adjust main.py

--- src/gencontent.py
+++ src/gencontent.py
@@ -31,3 +31,13 @@ def generate_page(from_path, template_path, dest_path):
         os.makedirs(dest_dir_path, exist_ok=True)
     to_file = open(dest_path, "w")
     to_file.write(template)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for dir in os.listdir(dir_path_content):
+        dir_path = os.path.join(dir_path_content, dir)
+        if os.path.isdir(dir_path):
+            generate_pages_recursive(dir_path, template_path, os.path.join(dest_dir_path, dir))
+        elif dir.endswith(".md"):
+            dest_file_name = dir[:-3] + ".html"
+            dest_file_path = os.path.join(dest_dir_path, dest_file_name)
+            generate_page(dir_path, template_path, dest_file_path)
