29f662d439edc646182e79daf7b40ae91a91ac12
Mark <marksreese@gmail.com>
2026-02-19 22:26:56 -0800

    change local script to use new dir name and fix base_path scope error in main.py

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from email.mime import base
 import os, sys, shutil
 from copystatic import copy_files_recursive
 from gencontent import generate_pages_recursive
@@ -6,11 +7,13 @@ dir_path_static = "./static"
 dir_path_public = "./docs" # by default, GitHub Pages serves sites from the "docs" directory of branch main
 dir_path_content = "./content"
 template_path = "./template.html"
-base_path = "/"
 
 def main():
-    if sys.argv[0]:
+    base_path = "/"
+    if sys.argv[0] != "src/main.py":
+        print(sys.argv[0])
         base_path = sys.argv[0]
+    
     print("Clearing files in public directory...")
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
